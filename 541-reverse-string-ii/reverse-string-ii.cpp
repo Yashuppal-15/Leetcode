372eba9991c6de887c8664e6b027e686a72adb3e
Yash Uppal <2k22.cse.2213230@gmail.com>
2025-06-24 16:29:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (76.39%) - LeetSync

---
+++ 541-reverse-string-ii/reverse-string-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int n = s.size();
+
+        for (int i = 0; i < n; i += 2 * k) {
+            // Reverse the first k characters in the current 2k block
+            // If less than k characters left, reverse all of them
+            int j = min(i + k, n);
+            reverse(s.begin() + i, s.begin() + j);
+        }
+
+        return s;
+    }
+};
