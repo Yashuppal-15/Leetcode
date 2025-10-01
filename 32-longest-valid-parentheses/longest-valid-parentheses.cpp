85fd742d2301c2a61bc8d5e4e669cc3c5a11d40e
Yash Uppal <2k22.cse.2213230@gmail.com>
2025-10-01 10:06:52 +0530

    Time: 4 ms (20.86%) | Memory: 11.8 MB (32.96%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
+
+        //Stack will store indices (not characters).
         stack<int> st;
+        //We push -1 initially as a base index to help compute lengths correctly.
+        //Example: If the string starts with a valid substring "()" at indices [0,1], 
+        //then 1 - (-1) = 2 (correct length).
         st.push(-1);
         int maxLen = 0;
 
+        //Iterate through each character with its index i.
         for(int i=0; i<s.length(); i++){
             char ch = s[i];
+            //If current char is '(', push its index onto the stack.
             if(ch == '('){
                 st.push(i);
             }
             else{
+                //If ')', we try to match it with the last '(' index in the stack → so we pop.
                 st.pop();
+
+                //If stack is empty after popping, push the current index of ')'.
+                //Why? Because this marks the new base index for future valid substrings.
+                //Example: In string ")()", when the first ')' is at index 0 → push 0 
+                //so the next valid substring can start after it.
                 if(st.empty()){
                     st.push(i);
                 }
                 else{
+                    //If stack is not empty:
+                    //The top element of stack is the index before the valid substring.
                     int len = i-st.top();
                     maxLen = max(len,maxLen);
                 }
