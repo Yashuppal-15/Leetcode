f3d4ac9f7178157c7c9e303b07f93b705993cc1d
Yash Uppal <2k22.cse.2213230@gmail.com>
2025-10-01 09:15:37 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (49.91%) - LeetSync

--- 394-decode-string/decode-string.cpp
+++ 394-decode-string/decode-string.cpp
@@ -2,41 +2,57 @@ class Solution {
 public:
     string decodeString(string s) {
         stack<string> st;
+
+        //Iterate through each character
         for(auto ch:s){
+            //When we see ], it means we’ve reached the end of an encoded block, so now we must decode it.
             if(ch == ']'){
                 string stringToRepeat = "";
+
+                //Pop characters/substrings until we find the opening bracket "[".
                 while(!st.empty() && st.top() != "["){
                     string top = st.top();
                     stringToRepeat += top;
                     st.pop();
                 }
+                //Discard the "[".
                 st.pop();
+
+                //Collect all digits from the stack.
                 string numericTimes = "";
                 while(!st.empty() && isdigit(st.top()[0])){
                     numericTimes += st.top();
                     st.pop();
                 }
+                //Reverse the digits (since they were popped in reverse order).
                 reverse(numericTimes.begin(), numericTimes.end());
-                int n = stoi(numericTimes);
+                int n = stoi(numericTimes);                 //Convert to integer n (number of times to repeat).
 
-                //final decoding
+
+                //Build the decoded substring
                 string currentdecode = "";
                 while(n--){
                     currentdecode += stringToRepeat;
                 }
+                //Push the result back on the stack.
                 st.push(currentdecode);
             }
+
+            //If the current char is not ], just push it as a string (either digit, letter, or "[").
             else{
                 string temp(1,ch);
                 st.push(temp);
             }
         }
+        
+        //Collect everything left in the stack into ans
         string ans;
         while(!st.empty()){
             ans += st.top();
             st.pop();
         }
+        //Reverse it because stack gave us reversed order.
         reverse(ans.begin(),ans.end());
-        return ans;
+        return ans;                                        //Return the decoded string.
     }
 };
