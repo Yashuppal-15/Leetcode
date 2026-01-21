9a119dc826403dbbd159db5f5ab7e3a7beca9550
Yash Uppal <2k22.cse.2213230@gmail.com>
2026-01-21 13:56:19 +0530

    Time: 788 ms (5.10%) | Memory: 35.8 MB (29.85%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    int bruteforce(vector<int>& nums) {
-        for(int i=0; i<nums.size();i++){
-            int lsum=0;
-            int rsum=0;
+    int bruteforce(vector<int>& nums) {                 // Brute force approach to find pivot index
+
+        for(int i = 0; i < nums.size(); i++){          // Iterate through each index and treat it as a potential pivot
+
+            int lsum = 0;                              // stores sum of elements on the left side
+            int rsum = 0;                              // stores sum of elements on the right side
         
-        for(int j=0; j<i; j++){
-            lsum+=nums[j];
+            for(int j = 0; j < i; j++){
+                lsum += nums[j];                       // Calculate left sum (elements before index i)
+            }
+
+            for(int j = i + 1; j < nums.size(); j++){
+                rsum += nums[j];                        // Calculate right sum (elements after index i)
+            }
+
+            if(lsum == rsum){                           // If left sum equals right sum, pivot index found
+                return i;
+            }
         }
-        for(int j=i+1; j<nums.size(); j++){
-            rsum+=nums[j];
-        }
-        if(lsum==rsum){
-            return i;
-        }
-        }
-        return -1;
+
+        return -1;                                      // If no pivot index satisfies the condition
     }
 
-
-    int pivotIndex(vector<int>& nums) {
+    int pivotIndex(vector<int>& nums) {                 // Function called by LeetCode to return pivot index
         return bruteforce(nums);
     }
 };
