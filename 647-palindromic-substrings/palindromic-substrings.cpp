e3bafa03d8185d6eae6088f070bdce575dbb6f59
Yash Uppal <2k22.cse.2213230@gmail.com>
2026-01-24 14:35:17 +0530

    Time: 11 ms (55.86%) | Memory: 21.2 MB (31.51%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,25 +1,45 @@
 class Solution {
 public:
-    int countSubstrings(string s) {
-        int res = 0;
+    // expand function
+    // i = left pointer
+    // j = right pointer
+    // this function counts palindromes by expanding from center
+    int expand ( string s , int i , int j )
+    {
+        int count = 0 ;
 
-        for (int i = 0; i < s.length(); i++) {
-            res += count_palindrome(s, i, i);
-            res += count_palindrome(s, i, i + 1);
+        // jab tak substring palindrome ho tab tak me count inc karunga, otherwise loop rok dunga
+        while ( i >= 0 && j < s.length ( ) && s[i] == s[j] )
+        {
+            count ++ ;   // ek palindrome mila
+            i -- ;       // left expand
+            j ++ ;       // right expand
         }
 
-        return res;        
+        return count ;
     }
 
-private:
-    int count_palindrome(string s, int left, int right) {
-        int count = 0;
-        while (left >= 0 && right < s.length() && s[left] == s[right]) {
-            count++;
-            left--;
-            right++;
+    // main function to count palindromic substrings
+    int countSubstrings ( string s )
+    {
+        int totalCount = 0 ;
+        int center = 0 ;
+
+        while ( center < s.length ( ) )
+        {
+            int oddCount = expand ( s , center , center ) ;        // step 1 : odd length palindrome
+            int evenCount = expand ( s , center , center + 1 ) ;    // step 2 : even length palindrome
+            totalCount = totalCount + oddCount + evenCount ;        // step 3 : total count
+
+            center ++ ;                                            // increase center
         }
-        return count;
-    }
 
+        return totalCount ;
+    }
 };
+
+/*
+T.C = O(N)
+S.C = O(1)
+N = length of string s
+*/
